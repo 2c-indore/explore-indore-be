79e97930888d1e5422f49f8ed2f35f3fb234b154
drklrd <srvbhattarai@gmail.com>
2019-07-30 13:38:10 +0545

    update snapshot and fix iport script bug

--- src/scripts/importData.js
+++ src/scripts/importData.js
@@ -11,68 +11,64 @@ const amenities = config.amenities;
 
 const combinedAmenites = ['public_clinics'];
 const accumulator = {};
+const callRate = 5000; // delay consecutive request by this much seconds. Just to ensure that there is certain time gap between consecutive API request
 
-// const fileMapping = {
-// 	"public_hospitals": "Public Hospitals",
-// 	"private_hospitals": "Private Hospitals",
-// 	"public_clinics": "Public Clinics",
-// 	"private_clinics": "Private Clinics",
-// 	"dentists": "Dentists",
-// 	"veterinaries": "Veterinaries",
-// 	"patho_radio_labs": "Pathology and Radiology Labs",
-// 	"anganwadi": "Anganwadis",
-// 	"blood_banks": "Blood Banks",
-// 	"mental_health_centers": "Mental Health Centers",
-// 	"bus_stops": "Bus Stops"
-// };
-
-dataImporter({
-	spreadsheetId:'1TaRj3jg3nvAy42GHa1FKKJsHot4EHdHssH2YyKIiwKg',
-	worksheet : amenities.map((amenity)=>{
-		return amenity.sheetName;
-	})
-})
-.then((result)=>{
-	amenities.forEach((amenity,index)=>{
-		const data = result[index];
-		data.forEach((element)=>{
-			if(typeof element.longitude === "number" && typeof element.latitude === "number"){
-				const elementCoordinates = turf.point([element.longitude, element.latitude]);
-				wardBoundaries.features.forEach((feature) => {
-					const isInside = turf.booleanPointInPolygon(elementCoordinates, feature);
-					if (isInside) {
-						element.ward_no = feature.properties.ward_no;
-						element.ward_name = feature.properties.ward_name;
+function asyncFunction(amenity, cb) {
+	console.log(`Waiting for ${callRate/1000} seconds.... -`, amenity.sheetName);
+	setTimeout(() => {
+		dataImporter({
+				spreadsheetId: '1TaRj3jg3nvAy42GHa1FKKJsHot4EHdHssH2YyKIiwKg',
+				worksheet: amenity.sheetName
+			})
+			.then((result) => {
+				// const data = result[index];
+				const data = result;
+				data.forEach((element) => {
+					if (typeof element.longitude === "number" && typeof element.latitude === "number") {
+						const elementCoordinates = turf.point([element.longitude, element.latitude]);
+						wardBoundaries.features.forEach((feature) => {
+							const isInside = turf.booleanPointInPolygon(elementCoordinates, feature);
+							if (isInside) {
+								element.ward_no = feature.properties.ward_no;
+								element.ward_name = feature.properties.ward_name;
+							}
+						});
 					}
 				});
-			}
-		});
-		if(amenity.key.indexOf(combinedAmenites) === -1){
-			const json = {
-				[amenity.key] : data
-			};
-			fs.writeFileSync(`${appRootPath}/src/data_snapshots/${amenity.key}.json`, JSON.stringify(json));
-		}else{
-			if(!accumulator[amenity.key]){
-				accumulator[amenity.key] = []
-			}
-			accumulator[amenity.key] = accumulator[amenity.key].concat(data);
-		}
-	});
+				if (amenity.key.indexOf(combinedAmenites) === -1) {
+					const json = {
+						[amenity.key]: data
+					};
+					fs.writeFileSync(`${appRootPath}/src/data_snapshots/${amenity.key}.json`, JSON.stringify(json));
+				} else {
+					if (!accumulator[amenity.key]) {
+						accumulator[amenity.key] = []
+					}
+					accumulator[amenity.key] = accumulator[amenity.key].concat(data);
+				}
+				console.log("Done - ", amenity.name);
+				cb();
 
+			})
+
+	}, callRate);
+}
+
+let requests = amenities.reduce((promiseChain, amenity) => {
+	return promiseChain.then(() => new Promise((resolve) => {
+		asyncFunction(amenity, resolve);
+	}));
+}, Promise.resolve());
+
+requests.then(() => {
 	const areThereGroupAmenties = Object.keys(accumulator).length;
-	if(areThereGroupAmenties > 0){
-		for(let group in accumulator){
+	if (areThereGroupAmenties > 0) {
+		for (let group in accumulator) {
 			const jsonObj = {
-				[group] : accumulator[group]
+				[group]: accumulator[group]
 			}
 			fs.writeFileSync(`${appRootPath}/src/data_snapshots/${group}.json`, JSON.stringify(jsonObj));
-		}	
+		}
 	}
-
-	console.log("Done!!!");
-
-})
-.catch((err)=>{
-	console.log("Error",err);
-})
+	console.log("Done with Snapshot creation")
+});
